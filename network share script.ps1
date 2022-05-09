3c2ac01e17294ad2f340ce70c485598ff653efca
DevonStephens416 <105228028+DevonStephens416@users.noreply.github.com>
2022-05-09 14:20:50 -0400

    Update network share script.ps1

--- network share script.ps1
+++ network share script.ps1
@@ -11,5 +11,5 @@ $sharePath = read-host “Type file path EX: C:\dc\usershare”
 #prompts user for input for the share file path
 $userName = read-host “Enter user for admin access”
 #prompts user for username for full SMB access
-$user = “Adatum\” + $username
+$user = “<put your orginazations domain here>\” + $username
 New-smbshare -name $shareName -path $sharePath -fullaccess $user
