d5c5c6047e49615150917a246dd04f687a393e87
DevonStephens416 <105228028+DevonStephens416@users.noreply.github.com>
2022-05-09 14:19:18 -0400

    Add files via upload

---
+++ network share script.ps1
@@ -0,0 +1,15 @@
+﻿#create a network share 
+#variables
+$shareName #stores user input for network share folder
+$sharePath #stores user input for network share path
+$userName # stores username
+$user
+#begin actual script below
+$shareName = read-host “Name network share” 
+#prompts user for input for the share name and stores that info in variable
+$sharePath = read-host “Type file path EX: C:\dc\usershare”
+#prompts user for input for the share file path
+$userName = read-host “Enter user for admin access”
+#prompts user for username for full SMB access
+$user = “Adatum\” + $username
+New-smbshare -name $shareName -path $sharePath -fullaccess $user
